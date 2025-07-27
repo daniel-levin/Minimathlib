edbbfe07b80fb5ec3daeaa0779fd40c734cf30ea
Daniel Levin <daniellevin2607@gmail.com>
2025-07-27 13:21:59 +0200

    remove debris

--- Main.lean
+++ Main.lean
@@ -1,4 +1,4 @@
 import Minimathlib
 
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  IO.println "Hello world"
--- Minimathlib.lean
+++
@@ -1,3 +0,0 @@
--- This module serves as the root of the `Minimathlib` library.
--- Import modules here that should be built as part of the library.
-import Minimathlib.Basic
